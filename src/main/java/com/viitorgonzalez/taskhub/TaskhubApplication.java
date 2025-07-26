fd9fc3d104ee0f34315d5c8dd4bedcbd21042e31
viitorgonzalez <pastor123.vs@gmail.com>
2025-07-26 09:31:35 -0300

    initial-commit

---
+++ src/main/java/com/viitorgonzalez/taskhub/TaskhubApplication.java
@@ -0,0 +1,13 @@
+package com.viitorgonzalez.taskhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viitorgonzalez/taskhub/TaskhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viitorgonzalez.taskhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
