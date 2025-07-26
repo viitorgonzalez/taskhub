bc1baab39db87202f2c9f9fc20ef24ae4004fddf
viitorgonzalez <pastor123.vs@gmail.com>
2025-07-26 13:50:21 -0300

    update: src\main\java\com\viitorgonzalez\taskhub\controller\TaskController.java

--- src/main/java/com/viitorgonzalez/taskhub/controller/TaskController.java
+++ src/main/java/com/viitorgonzalez/taskhub/controller/TaskController.java
@@ -1,12 +1,18 @@
 package com.viitorgonzalez.taskhub.controller;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.viitorgonzalez.taskhub.model.Task;
 import com.viitorgonzalez.taskhub.service.TaskService;
@@ -19,21 +25,43 @@ import com.viitorgonzalez.taskhub.service.TaskService;
 @RequestMapping("/task")
 public class TaskController {
 
-    // injeção de dependência
     private final TaskService taskService;
 
     public TaskController(TaskService taskService) {
         this.taskService = taskService;
     }
 
-    @GetMapping
-    public List<Task> getAll() {
-        return taskService.getAll();
+    @GetMapping()
+    public ResponseEntity<List<Task>> findAll() {
+        List<Task> tasks = this.taskService.findAll();
+        return ResponseEntity.ok(tasks);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> findById(@PathVariable Long id) {
+        Task task = taskService.findById(id);
+        return ResponseEntity.ok(task);
     }
 
     @PostMapping
-    public Task create(@RequestBody Task task) {
-        return taskService.save(task);
+    public ResponseEntity<Void> save(@RequestBody Task task) {
+        this.taskService.save(task);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+            .path("/{id}").buildAndExpand(task.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@RequestBody Task task, @PathVariable Long id) {
+        task.setId(id);
+        this.taskService.update(task);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        this.taskService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
